92f585fd3d13ab57d10a28b39dd409cebedc4aab
Aleksandar Lukic <acafineti@gmail.com>
2022-09-06 23:40:01 +0200

    flutter book

---
+++ fooderlich/lib/home.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/card2.dart';
+import 'home.dart';
+import 'card1.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = [
+    const Card1(),
+    const Card2(),
+    Container(color: Colors.blue),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Fooderlich",
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card 2',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card 3',
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ fooderlich/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/home.dart';
+import 'fooderlich_theme.dart';
+
+void main() {
+  // 1
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.dark();
+    return MaterialApp(
+      theme: theme,
+      title: 'Fooderlich',
+      home: const Home(),
+    );
+  }
+}
